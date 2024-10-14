1a9ae626c2da564418be8e10f03863b727544bc3
leavaring <lea.varing@efrei.net>
2024-10-14 17:06:39 +0200

    Load the CIFAR-10 dataset

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-#Load the CIFAR-10 dataset
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+
+import tensorflow as tf
+
+# Load the CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+# Display the shape of the training and test sets
+print("Training data shape (images):", x_train.shape)
+print("Training data shape (labels):", y_train.shape)
+print("Test data shape (images):", x_test.shape)
+print("Test data shape (labels):", y_test.shape)

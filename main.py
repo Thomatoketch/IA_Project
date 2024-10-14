4160dfe633708551d062739479bcee4322be0d0c
leavaring <lea.varing@efrei.net>
2024-10-14 17:10:18 +0200

    END task 1

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ print("Training data shape (images):", x_train.shape)
 print("Training data shape (labels):", y_train.shape)
 print("Test data shape (images):", x_test.shape)
 print("Test data shape (labels):", y_test.shape)
+
+# Number of training and test samples
+num_train_samples = x_train.shape[0]
+num_test_samples = x_test.shape[0]
+
+print("Number of training samples:", num_train_samples)
+print("Number of test samples:", num_test_samples)
+
+# Shape of each image
+image_shape = x_train.shape[1:]
+
+print("Shape of each image:", image_shape)
+print("Number of color channels:", image_shape[-1])
